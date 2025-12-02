3906ae9204dd78138da640c8857588cbbb812299
Vitaly Shishov <shishov_84@mail.ru>
2025-12-02 17:10:28 +0300

    create folder pkg

--- cmd/storage/main.go
+++ cmd/storage/main.go
@@ -10,10 +10,26 @@ import (
 func main() {
 	store := storage.NewStorage()
 
-	file, err := store.Upload("test.txt", []byte("hello man!"))
-	if err != nil {
-		log.Fatal(err)
+	file1, err1 := store.Upload("test.txt", []byte("hello man!"))
+	if err1 != nil {
+		log.Fatal(err1)
+	}
+	file2, err2 := store.Upload("test2.txt", []byte("hello man!!!"))
+	if err2 != nil {
+		log.Fatal(err1)
+	}
+	file3, err3 := store.Upload("test3.txt", []byte("hello woman!"))
+	if err3 != nil {
+		log.Fatal(err1)
 	}
 
-	fmt.Println("Upload file", file)
+	fmt.Println("Upload file", file1)
+	fmt.Println("Upload file", file2)
+	fmt.Println("Upload file", file3)
+
+	foundFile, ok := store.GetByID(file3.ID)
+	if ok != nil {
+		log.Fatal(ok)
+	}
+	fmt.Println("Found file", foundFile)
 }
--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,15 +1,40 @@
 package storage
 
-import "github.com/ShishVi/storage/internal/file"
+import (
+	"fmt"
+
+	"github.com/ShishVi/storage/internal/file"
+	"github.com/google/uuid"
+)
 
 type Storage struct {
+	Files map[uuid.UUID]*file.File
 }
 
 func NewStorage() *Storage {
-	return &Storage{}
+	return &Storage{
+		Files: make(map[uuid.UUID]*file.File),
+	}
 }
 
 func (s *Storage) Upload(filename string, blob []byte) (*file.File, error) {
-	return file.NewFile(filename, blob)
+	newFile, err := file.NewFile(filename, blob)
 
+	if err != nil {
+		return nil, err
+	}
+
+	s.Files[newFile.ID] = newFile
+
+	return newFile, nil
+
+}
+
+func (s *Storage) GetByID(fileId uuid.UUID) (*file.File, error) {
+
+	foundFile, ok := s.Files[fileId]
+	if !ok {
+		return nil, fmt.Errorf("File %v not found!", fileId)
+	}
+	return foundFile, nil
 }
